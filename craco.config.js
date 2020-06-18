4cae114d3fc52486960fe8e14f8ab3dd4de59331
Ahmed Tadde <ahmedtadde@gmail.com>
2020-06-17 22:53:29 -0400

    mongo realm app set up

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const CracoAntDesignPlugin = require("craco-antd");
 const CracoAlias = require("craco-alias");
 
@@ -22,5 +23,19 @@ const plugins = [
 ];
 
 module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@datatypes": path.resolve(__dirname, "src/datatypes"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@programs": path.resolve(__dirname, "src/programs"),
+      "@views": path.resolve(__dirname, "src/views"),
+      "@config": path.resolve(__dirname, "src/config"),
+    },
+    /* Any webpack configuration options: https://webpack.js.org/configuration */
+    configure: {
+      output: { path: path.resolve(__dirname, "build") },
+    },
+  },
   plugins,
 };
